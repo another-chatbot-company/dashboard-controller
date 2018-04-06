fba8cde13e28f8bb1d10432334f06d1075ca4b3d
Another Chatbot Company <senai.projeto.fepweb@gmail.com>
2018-04-05 21:35:01 -0300

    [Cesar] removendo info de porta

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const http  = require('http');
 const app = require('./app');
 const env = require('dotEnv').config();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 const server = http.createServer(app);
 
